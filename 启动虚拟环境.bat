9a1947ff1d0f086dbe9016421ee517d3e75291f0
liuke <lk6g@foxmail.com>
2019-11-19 12:12:01 +0800

    a

--- 启动虚拟环境.bat
+++
@@ -1,1 +0,0 @@
-cmd /k ll_env\Scripts\activate
--- 生成虚拟环境.bat
+++
@@ -1,5 +0,0 @@
-python -m venv ll_env
-copy ll_env\Scripts\activate.bat
-echo python manage.py runserver>>activate.bat
-rename activate.bat 运行本地服务器.bat
-pause
